f5e9ea71435fa8e288c3c0ce2e01e60156470f51
foo87b <foo87b@users.noreply.github.com>
2018-05-02 16:01:05 +0900

    Create Bot's skeleton

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord.Commands;
 
 namespace CCWallet.DiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private static ManualResetEvent WaitHandle { get; } = new ManualResetEvent(false);
+        private static IServiceProvider ServiceProvider { get; set; }
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CancelKeyPress += OnCancelKeyPress;
+            ServiceProvider = ConfigureServices().BuildServiceProvider();
+
+            try
+            {
+                var config = ServiceProvider.GetService<Services.Configure>();
+                var discord = ServiceProvider.GetService<DiscordSocketClient>();
+
+                await discord.LoginAsync(TokenType.Bot, config.DiscordToken);
+                await discord.StartAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+#if DEBUG
+                Console.WriteLine("Press Any Key To Exit...");
+                Console.Read();
+#endif
+                Environment.Exit(1);
+            }
+
+            WaitHandle.WaitOne();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<Services.Configure>()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig()
+                {
+                    DefaultRetryMode = RetryMode.AlwaysRetry,
+                }));
+        }
+
+        private static async void OnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            try
+            {
+                var discord = ServiceProvider.GetService<DiscordSocketClient>();
+
+                if (discord.LoginState == LoginState.LoggedIn ||
+                    discord.LoginState == LoginState.LoggingIn)
+                {
+                    await discord.LogoutAsync();
+                }
+
+                if (discord.ConnectionState == ConnectionState.Connected ||
+                    discord.ConnectionState == ConnectionState.Connecting)
+                {
+                    await discord.StopAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            e.Cancel = true;
+            WaitHandle.Set();
         }
     }
 }
---
+++ DiscordBot/Services/ConfigureService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DotNetEnv;
+using System.IO;
+
+namespace CCWallet.DiscordBot.Services
+{
+    public class ConfigureService
+    {
+        public const string DotEnvFile = ".env";
+
+        internal string DiscordToken => Env.GetString("DISCORD_TOKEN") ?? throw new InvalidOperationException();
+
+        public ConfigureService(string directory = null)
+        {
+            var path = Path.Combine(directory ?? Directory.GetCurrentDirectory(), DotEnvFile);
+
+            Load(path);
+        }
+
+        private void Load(string path)
+        {
+            if (File.Exists(path))
+            {
+                Env.Load(path);
+            }
+        }
+    }
+}
+
