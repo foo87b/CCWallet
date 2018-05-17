5a2a4bc1619048be27dbbabb7c0e076c23a98ced
foo87b <foo87b@users.noreply.github.com>
2018-05-17 16:12:51 +0900

    Fixed a fatal problem concerning secret key generation.

--- DiscordBot/Services/WalletService.cs
+++ DiscordBot/Services/WalletService.cs
@@ -49,9 +49,9 @@ namespace CCWallet.DiscordBot.Services
             return new KeyPath(new uint[]
             {
                 0x80000000 | 0, // service_index: discord is 0
-                0x80000000 | Convert.ToUInt32(id & 0xFFFF000000000000 >> 48), // user_id1: 16bit
-                0x80000000 | Convert.ToUInt32(id & 0x0000FFFFFF000000 >> 24), // user_id2: 24bit
-                0x80000000 | Convert.ToUInt32(id & 0x0000000000FFFFFF >>  0), // user_id3: 24bit
+                0x80000000 | Convert.ToUInt32((id & 0xFFFF000000000000) >> 48), // user_id1: 16bit
+                0x80000000 | Convert.ToUInt32((id & 0x0000FFFFFF000000) >> 24), // user_id2: 24bit
+                0x80000000 | Convert.ToUInt32((id & 0x0000000000FFFFFF) >>  0), // user_id3: 24bit
                 0x80000000 | 0, // reserved
             });
         }
